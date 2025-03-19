53ed17cffe40b871a3b8d866642db28781419a90
Marcos Silva <marcos.silva@yaman.com.br>
2025-03-19 16:36:08 -0300

    Order, Instant, ISO 8601 - PART 1

---
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,96 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+	// Lembrando para criar uma entidade
+	// attributes // Basic attributes // assosciations
+	//constructor // getters & setters // hashCode & equal (somente id) // serializable
+		
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	
+	// associations: relacionamento entre pedido e cliente (order e client)
+	@ManyToOne // aqui devo ler os papeis no diagrama, o "*" muitos, está do lado do pedido 
+	@JoinColumn(name = "cliente_id") // nome do atributo da associação
+	private User client;
+	
+	public Order() {
+		
+	}
+	
+	
+	
+	public Order(Long id, Instant moment, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+	
+	
+}

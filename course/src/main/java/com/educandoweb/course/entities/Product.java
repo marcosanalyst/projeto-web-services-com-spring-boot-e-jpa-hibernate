51b18d5112cdde41854ac99b8b96455044fa8d3a
Marcos Silva <marcos.silva@yaman.com.br>
2025-03-26 17:15:33 -0300

    Product-OrderItem one-to-many association

--- course/src/main/java/com/educandoweb/course/entities/Product.java
+++ course/src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -34,6 +37,9 @@ public class Product implements Serializable {
 	joinColumns =@JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id")) // qual vai ser o nome da tabela e quais serão as chaves estrangeiras que vai associar a tabela de produco com a tabela de categoria!
 	private Set<Category> categories = new HashSet<>(); // conjunto, o mesmo produto não pode ter mais de uma categoria de uma vez
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	private Product () {
 		
@@ -94,7 +100,16 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	}
-
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x: items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
